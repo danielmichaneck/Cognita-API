1e8283f8c4cf70a952a3d89257bdf2e00da23b6e
danielmichaneck <daniel.michaneck@gmail.com>
2024-10-01 15:28:17 +0200

    UserDto update.

--- Cognita-Tests/AuthControllerTests.cs
+++ Cognita-Tests/AuthControllerTests.cs
@@ -132,7 +132,7 @@ namespace Cognita_Tests
             var expired = CheckTokenIsValid(token.AccessToken);
 
             // ToDo: No no! /Dimitris
-            Thread.Sleep(2000);
+            Thread.Sleep(5000);
 
             var ttimeAfter = GetTokenExpirationTime(token.AccessToken);
 
