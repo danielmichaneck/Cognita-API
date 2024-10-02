f37fa5cfe83c4bc4a5bdfd1f89d3055cc0dd88d4
Victor Gustafsson <kerrik.vg@gmail.com>
2024-10-02 09:58:51 +0200

    Create_Module_Success_Test

--- Cognita-Tests/ModuleControllerTests.cs
+++ Cognita-Tests/ModuleControllerTests.cs
@@ -1,10 +1,13 @@
 ﻿using Cognita_API.Infrastructure.Data;
 using Cognita_Infrastructure.Models.Dtos;
 using Cognita_Infrastructure.Models.Entities;
+using Cognita_Shared.Dtos.Course;
+using Cognita_Shared.Dtos.Module;
 using IntegrationTests;
 using Microsoft.AspNetCore.Identity;
 using System.Net;
 using System.Net.Http.Headers;
+using System.Net.Http.Json;
 
 namespace Cognita_Tests
 {
@@ -53,6 +56,26 @@ namespace Cognita_Tests
             // Assert
 
             Assert.True(success);
+        }
+
+        [Fact]
+        public async Task Create_Module_Success_Test() {
+            // Arrange
+
+            var newModule = new ModuleForCreationDto() {
+                ModuleName = "Test Module 1",
+                Description = "Testing at Create_Module_Success_Test",
+                StartDate = DateOnly.MinValue,
+                EndDate = DateOnly.MaxValue
+            };
+
+            // Act
+
+            var response = await _httpClient.PostAsJsonAsync("api/courses/1/modules", newModule);
+
+            // Assert
+
+            Assert.True(response.IsSuccessStatusCode);
         }
     }
 }
