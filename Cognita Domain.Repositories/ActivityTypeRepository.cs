20bf67aa82f3d4010baf578b21f4b9c109e5325f
danielmichaneck <daniel.michaneck@gmail.com>
2024-10-03 16:13:10 +0200

    Merge fixes.

--- Cognita Domain.Repositories/ActivityTypeRepository.cs
+++ Cognita Domain.Repositories/ActivityTypeRepository.cs
@@ -1,12 +1,7 @@
-﻿using Cognita_API.Infrastructure.Data;
-using Cognita_Domain.Contracts;
+﻿using Cognita_Domain.Contracts;
+using Cognita_Infrastructure.Data;
 using Cognita_Shared.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Cognita_Domain.Repositories
 {
